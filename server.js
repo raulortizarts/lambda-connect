05e04f4a8cd363d38aafe4f1b5b49be48168d8b2
Raul Ortiz <raulortizars@gmail.com>
2019-11-06 20:01:38 -0600

    user validation

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const connectDB = require ('./config/db')
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-connectDB()
+connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
-// app.use('/api/users', require('./routes/api/users'))
-// app.use('/api/posts', require('./routes/api/posts'))
-// app.use('/api/profile', require('./routes/api/profile'))
-// app.use('/api/auth', require('./routes/api/auth'))
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
